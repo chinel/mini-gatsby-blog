b17c1e48f0cb6014e6111a9619e3b1805a471114
nelo <nelokwaz@gmail.com>
2020-10-08 17:13:28 +0100

    made some more corrections

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,7 +19,7 @@ const IndexPage = ({ data }) => (
     <SEO title="Home" />
     <div>
       <h1>My First Gatsby Blog</h1>
-      <h4>{data.allMarkdownRemark.totalCount}</h4>
+      <h4>{data.allMarkdownRemark.totalCount} posts</h4>
       {data.allMarkdownRemark.edges.map(({ node }) => (
         <div key={node.id}>
           <BlogLink to={node.fields.slug}>
